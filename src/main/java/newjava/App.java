612ed58810524b51fd94eab4c4b2b94a55560fe7
Sharat Kumar <sharatkumar31@gmail.com>
2019-12-15 19:43:40 +0530

    first commit

---
+++ src/main/java/newjava/App.java
@@ -0,0 +1,15 @@
+package newjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int a=10,b=23,c;
+	c=a+b;
+        System.out.println( "Sum Of Two Numbers : " +c );
+    }
+}
